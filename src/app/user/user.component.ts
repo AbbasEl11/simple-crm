560b6757385251d049a210d5cc04e007947b05d4
Abbas El <12345678+AbbasEl11@users.noreply.github.com>
2025-05-13 16:39:00 +0200

    added 2 routes - added matdialog - added form fields - added a user class

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
+import { MatNativeDateModule } from '@angular/material/core';
+import { User } from '../../models/user.class';
+
+
+@Component({
+  selector: 'app-user',
+  imports: [MatIconModule, MatButtonModule, MatTooltipModule, MatDialogModule, MatNativeDateModule],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.scss'
+})
+export class UserComponent {
+
+  user = new User({});
+
+  constructor(public dialog: MatDialog) {
+    
+   }
+
+  openDialog(): void {
+    this.dialog.open(DialogAddUserComponent)
+  }
+}
