40a928727a18a8aedc4f642d609b9154bfb4b2f6
Abbas El <12345678+AbbasEl11@users.noreply.github.com>
2025-05-14 18:01:45 +0200

    installed firebase

--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, NgZone, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatInputModule } from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { User } from '../../models/user.class';
 import { FormsModule } from '@angular/forms';
+import { Firestore, collection, addDoc } from '@angular/fire/firestore';
+
 
 
 @Component({
@@ -16,14 +18,37 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './dialog-add-user.component.scss'
 })
 export class DialogAddUserComponent {
-  user = new User({});
+  firestore: Firestore = inject(Firestore);
+  ngZone: NgZone = inject(NgZone); 
+  user = new User();
   birthDate!: Date;
 
 
-  saveUser() { 
-    this.user.birthDate! = this.birthDate!.getTime();
-    console.log(this.user);
-    
-  }
 
+  constructor() {}
+
+  saveUser() {
+    if (this.birthDate) {
+      this.user.birthDate = this.birthDate.getTime();
+    } else {
+      this.user.birthDate = 0; 
+    }
+
+    console.log(this.user);
+    this.ngZone.run(() => {
+      const usersCollection = collection(this.firestore, 'users');
+      addDoc(usersCollection, {
+        firstName: this.user.firstName,
+        lastName: this.user.lastName,
+        birthDate: this.user.birthDate,
+        street: this.user.street,
+        zipCode: this.user.zipCode,
+        city: this.user.city
+      }).then(() => {
+        console.log('User added successfully');
+      }).catch((error: any) => {
+        console.error('Error adding user: ', error);
+      });
+    });
+  }
 }
