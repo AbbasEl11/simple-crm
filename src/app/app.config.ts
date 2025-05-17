685041823093811120a27e86badcacd71eace52f
Abbas El <12345678+AbbasEl11@users.noreply.github.com>
2025-05-17 16:32:25 +0200

    added the table and the logo

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,10 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()), provideFirebaseApp(() => initializeApp({ projectId: "simple-crm-b2615", appId: "1:802308525370:web:c856cea51d16505c1b1403", storageBucket: "simple-crm-b2615.firebasestorage.app", apiKey: "AIzaSyDYaRPVhsvGYVWAn74Mo10LqwZuTEwAOFY", authDomain: "simple-crm-b2615.firebaseapp.com", messagingSenderId: "802308525370" })), provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase())]
 };
--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -1,6 +1,6 @@
 import { Component, NgZone, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { MatDialogModule } from '@angular/material/dialog';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDatepickerModule } from '@angular/material/datepicker';
@@ -8,12 +8,14 @@ import { MatNativeDateModule } from '@angular/material/core';
 import { User } from '../../models/user.class';
 import { FormsModule } from '@angular/forms';
 import { Firestore, collection, addDoc } from '@angular/fire/firestore';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import { CommonModule } from '@angular/common';
 
 
 
 @Component({
   selector: 'app-dialog-add-user',
-  imports: [MatDialogModule,MatButtonModule,MatInputModule,MatFormFieldModule,MatDatepickerModule,MatNativeDateModule,FormsModule],
+  imports: [MatDialogModule,MatButtonModule,MatInputModule,MatFormFieldModule,MatDatepickerModule,MatNativeDateModule,FormsModule,MatProgressBarModule, CommonModule],
   templateUrl: './dialog-add-user.component.html',
   styleUrl: './dialog-add-user.component.scss'
 })
@@ -22,10 +24,10 @@ export class DialogAddUserComponent {
   ngZone: NgZone = inject(NgZone); 
   user = new User();
   birthDate!: Date;
+  loading = false
 
 
-
-  constructor() {}
+  constructor(public dialogRef: MatDialogRef<DialogAddUserComponent>) {}
 
   saveUser() {
     if (this.birthDate) {
@@ -34,7 +36,7 @@ export class DialogAddUserComponent {
       this.user.birthDate = 0; 
     }
 
-    console.log(this.user);
+    this.loading = true;
     this.ngZone.run(() => {
       const usersCollection = collection(this.firestore, 'users');
       addDoc(usersCollection, {
@@ -46,9 +48,15 @@ export class DialogAddUserComponent {
         city: this.user.city
       }).then(() => {
         console.log('User added successfully');
+        this.loading = false;
+        this.dialogRef.close()
       }).catch((error: any) => {
         console.error('Error adding user: ', error);
       });
     });
+  }
+
+  close() {
+    this.dialogRef.close()
   }
 }
